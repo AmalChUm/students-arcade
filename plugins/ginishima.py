d5276ed048cea5ce7fe2ee65b133fd69d5937591
Ginishima <giniszyma@gmail.com>
2026-09-17 11:18:35 -0400

    Create ginishima.py
    Added ginishima.py to the repo, changing the functions seen in amal.roll

---
+++ plugins/ginishima.py
@@ -0,0 +1,11 @@
+import random
+
+# Each student names their file plugins/github_username.py
+
+AUTHOR = "Gaige Szy"
+APP_NAME = "Number Addition"
+
+def run():
+    """Main execution function called by main.py."""
+    total = 18 + 21
+    return f"Your total number is: {total}."
